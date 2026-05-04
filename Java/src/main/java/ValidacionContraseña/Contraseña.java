098cb62ae5b8334e57862bc14338578cf14ba4ad
Lemsito22 <lemquilca22@gmail.com>
2026-05-04 11:57:18 +0200

    TestsContraseñas

--- Java/src/main/java/ValidacionContraseña/Contraseña.java
+++ Java/src/main/java/ValidacionContraseña/Contraseña.java
@@ -21,6 +21,7 @@ public class Contraseña {
     public boolean minCaracteres(){
         return listacaracteres.size() >= 8;
     }
+
     public boolean tieneMayuscula(){
         for (char c : listacaracteres){
             if (Character.isUpperCase(c)){ //Este método comprueba si hay una mayúscula en el array
@@ -35,6 +36,17 @@ public class Contraseña {
             if (Character.isDigit(c)) return true; //Este método sirve para buscar numeros
         }
         return false;
+    }
+
+    public boolean noTieneEspacios(){
+        for (char c : listacaracteres){
+            if (Character.isWhitespace(c)) return false;
+        }
+        return true;
+    }
+
+    public boolean contraseñaSegura(){
+        return minCaracteres() && tieneMayuscula() && tieneNumero() && noTieneEspacios();
     }
 
 }
--- Java/src/main/java/ValidacionContraseña/TestContraseña.java
+++ Java/src/main/java/ValidacionContraseña/TestContraseña.java
@@ -19,4 +19,34 @@ public class TestContraseña {
         password.cargarContreña("Holamama");
         assertTrue(password.tieneMayuscula(),"Tu contraseña necesita minimo 1 caracter en Mayuscula");
     }
+    @Test
+    void testTieneNumero(){
+        Contraseña password = new Contraseña();
+
+        password.cargarContreña("contra1234");
+        assertTrue(password.tieneNumero(),"Tu contraseña necesita minimo 1 caracter numérico");
+    }
+
+    @Test
+    void testTieneEspacio(){
+        Contraseña password = new Contraseña();
+
+        password.cargarContreña("contra1234");
+        assertTrue(password.noTieneEspacios(),"Tu contraseña no debería tener espacios vacios");
+    }
+
+    @Test
+    void testContraseñaSegura(){
+        Contraseña password = new Contraseña();
+
+        password.cargarContreña("Tumb4l4c4s4m4m1");
+        assertTrue(password.contraseñaSegura(),"Para ser valida debe tener 1 caracter en Mayuscula, 1 valor numerico y tener un minimo de 8 caracteres y 0 espacios vacios");
+
+    }@Test
+    void testCadenaVacia(){
+        Contraseña password = new Contraseña();
+
+        password.cargarContreña("");
+        assertTrue(password.contraseñaSegura(),"Una cadena vacia no puede ser una contraseña segura");
+    }
 }
